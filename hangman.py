6f653f719f4ca5342665f7c456e5565ebb00766b
Sam <Sam@Dev.co.uk>
2020-01-03 14:04:29 +0000

    #6 implemented user being able to guess a letter, then added functionality to check if the letter is in the word, have also added a function that prints out the current game state.

--- hangman.py
+++ hangman.py
@@ -18,7 +18,13 @@ word_to_guess = words.select_random_word()
 length_of_word_to_guess = len(word_to_guess)
 
 # Sets the current game state as a string of _, the amount of underscores is the length of the random word.
-current_guess_state = "_" * length_of_word_to_guess
+current_guess_state = list("_" * length_of_word_to_guess)
 
 print("The word is", length_of_word_to_guess, "characters long.")
-print("The current guess is", current_guess_state)
+print("The current state is", words.print_current_state(current_guess_state))
+
+print("Guess a letter.", end=" ")
+guess = input()
+
+current_guess_state, turn_result = words.player_guess(word_to_guess, guess, current_guess_state)
+words.update_player(current_guess_state, turn_result)
--- words.py
+++ words.py
@@ -16,3 +16,33 @@ def select_random_word():
         Returns a random word from a pre-defined list
     """
     return random.choice(list_of_words)
+
+
+def player_guess(word_to_guess, guess, current_guess_state):
+    """
+
+    """
+    turn_result = False
+    for idx, x in enumerate(word_to_guess):
+        if guess == x or guess == x.swapcase():
+            current_guess_state[idx] = x
+            turn_result = True
+
+    return current_guess_state, turn_result
+
+
+def update_player(current_guess_state, turn_result):
+    """
+
+    """
+    if turn_result:
+        print("You were correct! The new state is:")
+        print_current_state(current_guess_state)
+    else:
+        print("You were incorrect!")
+    return
+
+
+def print_current_state(current_guess_state):
+    print(" ".join(current_guess_state))
+    return
