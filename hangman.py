5fc61d13f0309105fd270457128d8588ea208e98
Sam <Sam@Dev.co.uk>
2020-01-03 13:11:42 +0000

    #2 added list of random words and a function that selects a random word from the list. Also tells the player the amount of letters in the word. Also added a .gitignore

--- hangman.py
+++ hangman.py
@@ -1,5 +1,11 @@
+import words
+
 print("What is your name?")
 
 username = input()
 
 print("Hello " + username, "\nWelcome to a game of Hangman!")
+
+word_to_guess = words.select_random_word()
+
+print("The word is", len(word_to_guess), "characters long.")
