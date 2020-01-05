b62b4304e0d6cda6c432016dfc9c8d173ee88d4e
Sam <Sam@Dev.com>
2020-01-05 00:07:29 +0000

    #11 added ability to replay after winning or losing.

--- hangman.py
+++ hangman.py
@@ -3,49 +3,17 @@ import os
 import words
 
 
-# Game variables
-amount_of_lives = 10
-
 # Asks the player there name and stores it, then says hello to them.
 username = input("What is your name? ")
 print("Hello " + username, "\nWelcome to a game of Hangman!")
 
-# Gets a random word and calculates the length of the random word
-word_to_guess = words.select_random_word()
-length_of_word_to_guess = len(word_to_guess)
-# Sets the current game state as a string of _, the amount of underscores is the length of the random word.
-current_guess_state = list("_" * length_of_word_to_guess)
+program_loop = True
+while program_loop:
+    # Plays Hangman
+    words.play_hangman()
 
-# List to hold the users guesses
-players_guesses = []
-# While loop for while a game is running
-game_loop = True
-while game_loop:
-    print(word_to_guess)
-    words.print_current_game_state(length_of_word_to_guess, current_guess_state)
+    # Asks if the player wants to play again, and ends the loop if they don't
+    play_again = input("Would you like to play again? (y or n): ")
 
-    # Gets input from the player
-    players_guess = input("Guess a letter. ")
-    # Checks the players guess
-    current_guess_state, turn_result = words.check_players_guess(word_to_guess, players_guess, current_guess_state, players_guesses)
-    # Appends the guess to the list of already chosen guesses.
-    players_guesses.append(players_guess)
-
-    # Checks the result of the turn
-    if turn_result == "Correct":
-        print("You were correct!")
-    elif turn_result == "Repeat":
-        print("You used the letter '", players_guess, "' already.")
-    elif turn_result == "Wrong":
-        print("You were incorrect! You lose a life!")
-        amount_of_lives -= 1
-        # Checks if the user has any lives left
-        if amount_of_lives <= 0:
-            print("You have run out of lives.")
-            print("Game Over")
-            game_loop = False
-
-    # Checks if the the entire word is correctly guessed
-    if "".join(current_guess_state) == word_to_guess:
-        print("You have Won!")
-        game_loop = False
+    if play_again.lower() != "y":
+        program_loop = False
--- words.py
+++ words.py
@@ -51,3 +51,47 @@ def print_current_guess_state(current_guess_state):
     """
     print(" ".join(current_guess_state))
     return
+
+
+def play_hangman():
+    # Game variables
+    amount_of_lives = 10
+
+    # Gets a random word and calculates the length of the random word
+    word_to_guess = select_random_word()
+    length_of_word_to_guess = len(word_to_guess)
+    # Sets the current game state as a string of _, the amount of underscores is the length of the random word.
+    current_guess_state = list("_" * length_of_word_to_guess)
+
+    # List to hold the users guesses
+    players_guesses = []
+    # While loop for while a game is running
+    game_loop = True
+    while game_loop:
+        print_current_game_state(length_of_word_to_guess, current_guess_state)
+
+        # Gets input from the player
+        players_guess = input("Guess a letter. ")
+        # Checks the players guess
+        current_guess_state, turn_result = check_players_guess(word_to_guess, players_guess, current_guess_state, players_guesses)
+        # Appends the guess to the list of already chosen guesses.
+        players_guesses.append(players_guess)
+
+        # Checks the result of the turn
+        if turn_result == "Correct":
+            print("You were correct!")
+        elif turn_result == "Repeat":
+            print("You used the letter '", players_guess, "' already.")
+        elif turn_result == "Wrong":
+            print("You were incorrect! You lose a life!")
+            amount_of_lives -= 1
+            # Checks if the user has any lives left
+            if amount_of_lives <= 0:
+                print("You have run out of lives.")
+                print("Game Over")
+                game_loop = False
+
+        # Checks if the the entire word is correctly guessed
+        if "".join(current_guess_state) == word_to_guess:
+            print("You have Won!")
+            game_loop = False
